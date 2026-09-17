da47033e7db7c4d35a198ad5848663f28f20db38
gikera <gikerapenuel@gmail.com>
2026-09-17 17:23:47 +0300

    added the charging feature and comments

--- main.cpp
+++ main.cpp
@@ -10,27 +10,28 @@ int main(){
 
     while(true){
         
-        std::cout<< "\t WELCOME \t \t \n";
+        std::cout<< "\t WELCOME \t \t \n";          //main menu
+        std::cout<< "AVAILABLE SLOTS: "<< parkinglot.getEmptySlots()<<"\n"; 
         std::cout<< "\t 1. CHECK IN \n";
         std::cout<< "\t 2. CHECK OUT \n";
         std::cout<< "\t 3. SLOT INFO\n";
 
         std::cout<< "Enter a # choice: ";
-        int choice{};
+        int choice{};                               //accepts user info
         //std::cin>> choice;
 
-        if (!(std::cin >> choice)) {
+        if (!(std::cin >> choice)) {                //this checks if valid input has been added
             std::cout << "\nError: Invalid input format. Please enter a number.\n";
-            std::cin.clear(); // Resets the error flag
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discards bad input
+            std::cin.clear();                       // Resets the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // Discards bad input and input buffer
             continue;
         }
 
-        if(choice == 1 || choice == 2 || choice == 3){
+        if(choice == 1 || choice == 2 || choice == 3){          //ensures only valid choices are in
             switch(choice){
                 case 1: {
                     if(parkinglot.getEmptySlots() <= 0){
-                        std::cout<< "NO AVAILABLE SLOTS... RETURN LATER \n";
+                        std::cout<< "NO AVAILABLE SLOTS... RETURN LATER \n";        //does not continue if no slots available to check in
                     }
                     else{
                        
@@ -42,12 +43,14 @@ int main(){
                         std::cout<< "enter second name: ";
                         std::cin>> c.s_name;
 
-                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //ignores input buffer
 
-                        std::cout<< "enter vehicle number plate: ";
-                        std::getline(std::cin, c.number_plate);
+                        std::cout<< "enter vehicle number plate: ";     
+                        std::getline(std::cin, c.number_plate);                 //can accept a white space
 
-                        int slot = parkinglot.addCustomer(c);
+                        c.entryTime = std::chrono::steady_clock::now();         //records entry time to elemt in struct of customer
+
+                        int slot = parkinglot.addCustomer(c);                   //slot is assigned the index of customer slot in array
 
                         std::cout<< "\n you have been assigned SLOT #"<< slot<< "\n\n";
 
@@ -57,17 +60,39 @@ int main(){
                     break;
                 }
 
-                case 2: {
+                case 2: {                                               //for checking out
                     int slotno{};
                     std::cout<< "Enter your parking slot number #: ";
                     std::cin>> slotno;
 
-                    parkinglot.checkOut(slotno);
+                    /*
+                    auto currentTime = std::chrono::steady_clock::now();
+
+                    auto checkoutTime = parkinglot.getTime(slotno);
+
+                    auto elapsed = std::chrono::duration_cast<std::chrono::minutes> (currentTime - checkoutTime);
+
+
+                    int fees{};
+                    fees = parkinglot.charges(elapsed.count());
+
+                    
+
+                    
+                    if(parkinglot.checkOut(slotno)){
+                        std::cout << "\n time parked: " << elapsed.count() << "min\n";
+                        std::cout<< "THUS \n";
+                        std::cout<< "you are to pay KSH"<< fees;
+                    }
+                    */
+
+                    parkinglot.checkOut(slotno);                            //calls the check out function
+
                     break;
                 }
 
                 case 3: {
-                    for(int i = 0; i < parkinglot.getTotalSlot(); i++){
+                    for(int i = 0; i < parkinglot.getTotalSlot(); i++){             //prints all info about taken slots
                         parkinglot.printInfo(i);
                     }
 
--- main.hpp
+++ main.hpp
@@ -4,72 +4,133 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <chrono>
 
-const int Max_Customer{200};
+const int Max_Customer{200}; //using a const int as the number of customers is not more than 200
 
-struct Customer{
+struct Customer{                                        //using struct to store variable of different types
     std::string f_name{};
     std::string s_name{};
-    std::string number_plate{};
+    std::string number_plate{};                         //number plate is a string to store even letters
+    std::chrono::steady_clock::time_point entryTime;    //this stores the entry time of the car
+    //std::chrono::steady_clock::time_point exitTime;
     
 };
 
 struct Parking{
-    private:
+    private:                                            //private members to be accessed only in the struct
         int total_slots{};
-        std::vector<bool> slot_status;
-        std::vector<Customer> customers;
+        std::vector<bool> slot_status;                  //this assigns boolean values to vectors as slot status
+        std::vector<Customer> customers;                //this stores each customer information as a value in an array
 
     public:
         Parking(int totalSlots)
-            :total_slots(totalSlots), slot_status(totalSlots, true), customers(totalSlots){}
+            :total_slots(totalSlots), slot_status(totalSlots, true), customers(totalSlots){} //this is a constructor that sets totalslots
+                                                                                            //sets the value of the array equal to total
+                                                                                            // slots and all to be empty
+                                                                                            //number of customers allowed is equal to
+                                                                                            //number of totalslots
 
-        int getTotalSlot(){
-            return total_slots;
+
+        int getTotalSlot(){                                                                 //a function to see number of slots
+            return total_slots;                                 
         }
 
-        int getEmptySlots() const {
+        int getEmptySlots() const {                                                         //a function to get empty slots
             int count = 0;
-            for(bool empty : slot_status){
-                if(empty){
-                    ++count;
+            for(bool empty : slot_status){                                                  // a loop that sweeps all slot status 
+                if(empty){                                                                  //every slot that is true increases the count
+                    ++count;                                                                //the end result count is returned as no of e.slots
                 }
             }
             return count;
         }
 
-        int addCustomer(const Customer& newCustomer){
-            for(int i = 0; i < total_slots; i++){
-                if(slot_status[i]){
-                    slot_status[i] = false;
-                    customers[i] = newCustomer;
-                    return i; 
+        int addCustomer(const Customer& newCustomer){                   //a function to add new customer to first available slot
+            for(int i = 0; i < total_slots; i++){                       //a loop for scrolling through all slots
+                if(slot_status[i]){                                     //if the slotstatus is true which means empty
+                    slot_status[i] = false;                             //the slotstatus is set to false as it is now taken
+                    customers[i] = newCustomer;                         //the particular slot is assigned to new customer
+                    return i;                                           //slot number is returned to user
                 }
             }
             return -1;
         }
 
-        bool checkOut(int slotIndex){
-            if(slotIndex < 0 || slotIndex >= total_slots){
-                std::cout<< "ERR!! INVALID SLOT NUMBER \n";
+        int charges(int time){                                          //function to calculate the charges to be paid
+            int rate{};
+            if(time <= 30){
+                rate = 0;
+
+                return rate;
+            }
+
+            else if(time <= 120){
+                rate = 50;
+
+                return rate;
+            }
+
+            else if(time <=240){
+                rate = 100;
+
+                return rate;
+            }
+
+            else if(time <= 360){
+                rate = 300;
+
+                return rate;
+            }
+            else{
+                rate = 500;
+
+                return rate;
+            }
+
+        }
+
+        bool checkOut(int slotIndex){                           //function to checkout, set as bool so can return a true,
+            if(slotIndex < 0 || slotIndex >= total_slots){      //which means slot is now empty
+                std::cout<< "ERR!! INVALID SLOT NUMBER \n";         //this is displayed if slot entered does not exist
                 return false;
             }
             if(slot_status[slotIndex]){
-                std::cout<< "Slot #"<< slotIndex << " is already empty \n";
+                std::cout<< "Slot #"<< slotIndex << " is already empty \n";  //this is displayed if user try to checkout empty slot
                 return false;
             }
+
+            auto currentTime = std::chrono::steady_clock::now(); //time during checkout
+
+            //auto checkoutTime = parkinglot.getTime(slotno);
+                                                                                    //the elapsed time is calculated
+            auto elapsed = std::chrono::duration_cast<std::chrono::minutes> (currentTime - customers[slotIndex].entryTime);
+
+
+            int fees{};
+            fees = charges(elapsed.count());                    //fees is assigned the charges to be paid
+
+            std::cout << "\n time parked: " << elapsed.count() << "min\n";
+            std::cout<< "THUS \n";
+            std::cout<< "you are to pay KSH"<< fees<< "\n\n";
 
             std::cout<< "CAR: "<< customers[slotIndex].number_plate << " has checked out \n";
             std::cout<< "Slot no #"<< slotIndex << " is now empty";
 
-            slot_status[slotIndex] = true;
-            customers[slotIndex] = Customer{};
+            slot_status[slotIndex] = true;                  //slotstatus is set to true which means empty
+            customers[slotIndex] = Customer{};              //resets customer information in that slot
 
             return true;
             
         }
 
-        void printInfo(int slotIndex) const{
+        /*
+        auto getTime(int slotIndex){
+            return customers[slotIndex].entryTime;
+        }
+        */
+
+        void printInfo(int slotIndex) const{                            //prints all info of customer in slot x
             if(slotIndex >= 0 && slotIndex < total_slots){
                 if(!slot_status[slotIndex]){
                     std::cout<< "\n \t \t Slot no #" << slotIndex<< "\n";
@@ -78,7 +139,7 @@ struct Parking{
                     std::cout << customers[slotIndex].s_name << "\n";
                     std::cout << "\t \t PLATE: " << customers[slotIndex].number_plate << "\n";
                 }
-                else{
+                else{                                                   //prints if no customer is assigned there
                     std::cout<< "Slot no #"<< slotIndex << " is empty. \n";
                 }
             }
